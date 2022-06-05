d2c051238e3e0293c1d53e07b9f532065facf52f
Will Mitchell <will.mitchell@gmail.com>
2022-06-05 00:53:47 +0000

    customize shell env

--- utils/init-script.sh
+++ utils/init-script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "source /usr/share/bash-completion/completions/git" >> $HOME/.bashrc
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+cat $SCRIPT_DIR/aliases.sh >> $HOME/.bashrc
